95b943dd50a127fe368e60784ee7e5d730927c3c
panfen <2809956575@qq.com>
2017-12-11 22:35:21 +0800

    'update'

--- src/components/dashboard/PicManage.vue
+++ src/components/dashboard/PicManage.vue
@@ -2,22 +2,31 @@
 	<el-row class="subcontent">
 		<!-- S  topbar -->
 		<el-row class="topbar">
-			<el-col :span="6">
-				<el-input placeholder="图片名称" icon="search" v-model="picname" :on-icon-click="handleSearch"></el-input>
+			<el-col :span="18">
+				<el-col :span="8">
+					<el-input placeholder="图片名称" icon="search" v-model="picname" :on-icon-click="handleSearch"></el-input>
+				</el-col>
+				<el-button class="refresh-pic" icon="el-icon-refresh">换一批</el-button>
+				<span class="pic-amount">共 125 张</span>
 			</el-col>
-			<el-col :span="6" :offset="12">
-				<el-button type="success">上传图片</el-button>
+			<el-col :span="6">
+				<el-button type="success" id="upload-pic">上传图片</el-button>
+				<el-button type="primary" id="manage-pic">批量管理</el-button>
 			</el-col>
 		</el-row>
 		<!-- E topbar -->
 
 		<!-- pic list -->
 		<el-row class="piclist">
-			<ul>
-				<li v-for="(url,index) in urlList">
-					<img v-bind:src="url" width="200" height="120" @click="viewPicAct(index)"/>
-				</li>
-			</ul>
+			<el-checkbox-group v-model="checkedPics" @change="handleCheckedPicsChange">
+				<ul>
+					<li v-for="(url,index) in urlList" class="pic-wrap">
+						<img v-bind:src="url" width="200" height="120" @click="viewPicAct(index)"/>
+						<el-checkbox :key="url" class="pic-checkbox"></el-checkbox>
+					</li>
+				</ul>
+			</el-checkbox-group>
+			<el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
 		</el-row>
 
 		<!-- 弹出层 -->
@@ -42,14 +51,24 @@
 				picname: '',
 				currentIndex: 0,
 				currentUrl: '',
+				checkAll: false,
+				checkedPics: '',
+				isIndeterminate: true,
 				urlList: ['http://p3.so.qhimgs1.com/bdr/_240_/t01af075dc8036b8ad3.jpg',
-									'http://p2.so.qhmsg.com/bdr/_240_/t011c1f798ef18a7027.jpg',
+									'http://p0.so.qhimgs1.com/bdr/_240_/t01979009503370fd70.jpg',
 									'http://p1.so.qhimgs1.com/bdr/_240_/t01afb32fc2cfcdcedd.jpg',
-									'http://p3.so.qhmsg.com/bdr/_240_/t0148ae67d964960498.jpg',
-									'http://p1.so.qhmsg.com/bdr/_240_/t01449a7e508ef5e356.jpg',
-									'http://p2.so.qhmsg.com/bdr/_240_/t017bce9f966c9fb257.jpg',
+									'http://p5.so.qhimgs1.com/bdr/_240_/t0112343dc49d5ee49b.jpg',
+									'http://p1.so.qhimgs1.com/bdr/_240_/t0148896d9fe40741c1.jpg',
+									'http://p5.so.qhimgs1.com/bdr/_240_/t01dd8b291e0798717b.jpg',
+									'http://p3.so.qhimgs1.com/bdr/_240_/t0152e74cb98fec1b13.jpg',
+									'http://p1.so.qhimgs1.com/bdr/_240_/t015dfce6549888df7d.jpg',
+									'http://p1.so.qhimgs1.com/bdr/_240_/t01e0a34cdea51eca5d.jpg',
 									'http://p0.so.qhimgs1.com/bdr/_240_/t01e7cce6ca35c5ac8a.jpg',
-									'http://p5.so.qhimgs1.com/bdr/_240_/t01dd8b291e0798717b.jpg',],
+									'http://p0.so.qhimgs1.com/bdr/_240_/t01920928f47884b03a.jpg',
+									'http://p0.so.qhimgs1.com/bdr/_240_/t01e33a5b3f61ec98e4.jpg',
+									'http://p0.so.qhimgs1.com/bdr/_240_/t0136b18f5b94254eac.jpg',
+									'http://p1.so.qhimgs1.com/bdr/_240_/t0161daa340ff02dd81.jpg',
+									'http://p2.so.qhimgs1.com/bdr/_240_/t019d0aeac9b7fc1892.jpg'],
 			}
 		},
 		methods: {
@@ -66,13 +85,22 @@
       },
       prevPicAct(){
       	this.currentIndex = this.currentIndex-- <= 0 ? 0 : this.currentIndex--;
-      	console.log(this.currentIndex)
       	this.currentUrl = this.urlList[this.currentIndex];
       },
       nextPicAct(){
       	this.currentIndex = this.currentIndex++ >= this.urlList.length - 1 ? this.urlList.length - 1 : this.currentIndex++;
       	this.currentUrl = this.urlList[this.currentIndex];
-      }
+      },
+      handleCheckedPicsChange(value){
+      	console.log(value)
+      	var checkedCount = value.length;
+        this.checkAll = checkedCount === this.urlList.length;
+        this.isIndeterminate = checkedCount > 0 && checkedCount < this.urlList.length;
+      },
+      handleCheckAllChange(val) {
+        this.checkedPics = val ? this.urlList : [];
+        this.isIndeterminate = false;
+      },
     }
 	}
 </script>
@@ -81,13 +109,34 @@
 	.topbar .el-col:last-child{
 		text-align: right;
 	}
+	.refresh-pic{
+		margin: 0 6px;
+	}
+	.pic-amount{
+		font-size: 12px;
+	}
+	#manage-pic{
+		float: right;
+	}
+	#upload-pic{
+		margin-left: 6px;
+		float: right;
+	}
 	.piclist{
 		margin-top: 20px;
 	}
-	.piclist ul li{
+	.piclist ul li.pic-wrap{
+		width: 200px;
+		height: 120px;
 		cursor: pointer;
 		display: inline-block;
-		margin: 0 10px 10px 0;
+		margin: 0 6px 6px 0;
+		position: relative;
+	}
+	.piclist ul li.pic-wrap .pic-checkbox{
+		top: 4px;
+		right: 4px;
+		position: absolute;
 	}
 	.el-dialog__body{
 		position: relative;
