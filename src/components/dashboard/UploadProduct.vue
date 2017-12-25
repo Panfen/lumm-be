02f8855ffdd62e5a0a7b65c2223a7de3dc3e0763
panfen <2809956575@qq.com>
2017-12-25 15:34:01 +0800

    'update'

--- src/components/dashboard/UploadProduct.vue
+++ src/components/dashboard/UploadProduct.vue
@@ -16,14 +16,43 @@
 			<el-row class="baseinfo">
 				<h2>基础信息</h2>
 				<el-form :model="form" class="form">
-			    <el-form-item label="活动名称" :label-width="formLabelWidth">
-			      <el-input v-model="form.name" auto-complete="off"></el-input>
+			    <el-form-item label="商品名称" :label-width="formLabelWidth">
+			    	<el-col :span="6">
+			      	<el-input v-model="form.name" auto-complete="off"></el-input>
+			      </el-col>
 			    </el-form-item>
-			    <el-form-item label="活动区域" :label-width="formLabelWidth">
-			      <el-select v-model="form.region" placeholder="请选择活动区域">
-			        <el-option label="区域一" value="shanghai"></el-option>
-			        <el-option label="区域二" value="beijing"></el-option>
-			      </el-select>
+			    <el-form-item label="商品类别" :label-width="formLabelWidth">
+			    	<el-col :span="6">
+				      <el-select v-model="form.group" placeholder="请选择商品类别">
+				        <el-option label="猫粮" value="food"></el-option>
+				        <el-option label="猫砂" value="sand"></el-option>
+				        <el-option label="猫砂盆" value="basin"></el-option>
+				        <el-option label="猫窝" value="house"></el-option>
+				        <el-option label="玩具" value="toy"></el-option>
+				        <el-option label="猫窝" value="tool"></el-option>
+				      </el-select>
+			      </el-col>
+			    </el-form-item>
+			    <el-form-item label="商品进价" :label-width="formLabelWidth">
+			    	<el-col :span="6">
+			      	<el-input v-model="form.orig_price" auto-complete="off"></el-input>
+			      </el-col>
+			      <span class="yuan">元</span>
+			    </el-form-item>
+			    <el-form-item label="商品售价" :label-width="formLabelWidth">
+			    	<el-col :span="6">
+			      	<el-input v-model="form.sale_price" auto-complete="off"></el-input>
+			      </el-col>
+			      <span class="yuan">元</span>
+			    </el-form-item>
+			    <el-form-item label="库存" :label-width="formLabelWidth">
+			    	<el-col :span="6">
+			      	<el-input v-model="form.stock" auto-complete="off"></el-input>
+			      </el-col>
+			    </el-form-item>
+			    <el-form-item label="展示图片" :label-width="formLabelWidth">
+			    	<p>主图 <span class="info">(建议尺寸为640像素*640像素，大小不超过500kb)</span></p>
+			    	<div></div>
 			    </el-form-item>
 			  </el-form>
 			</el-row>
@@ -49,13 +78,10 @@
 				formLabelWidth: '120px',
 				form:{
 					name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          group: '',
+          orig_price: '',
+          sale_price: '',
+          stock: '',
 				}
 			}
 		},
@@ -71,12 +97,12 @@
 	.topbar h1{
 		font-size: 18px;
 		color: #000;
-		
 	}
 	.inputlist h2{
 		font-size: 16px;
 		position: relative;
 		padding-left: 18px;
+		margin: 10px 0;
 	}
 	.inputlist h2:before{
 		content: "";
@@ -88,5 +114,20 @@
 		position: absolute;
 		left: 0px;
 		top: 8px;
+	}
+	.el-form-item{
+		margin-bottom: 14px;
+	}
+	.el-select{
+		width: 100%;
+	}
+	.el-form-item__label{
+		padding: 0 24px 0 0;
+	}
+	span.yuan{
+		padding-left: 6px;
+	}
+	span.info{
+		color: #8d8d8d;
 	}
 </style>
