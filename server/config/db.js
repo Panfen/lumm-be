eea80dbd64f781639a964fa615796b9bf1b6abad
panfen <2809956575@qq.com>
2017-12-29 19:51:12 +0800

    'update'

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const LummDataBase = new Sequelize('mysql://root:@localhost/lumm');
+const Lumm = new Sequelize('mysql://root:@localhost/lumm');
 
 module.exports = {
-	LummDataBase
+	Lumm
 }
